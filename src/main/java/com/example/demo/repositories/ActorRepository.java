0a4bca822f008fe56bb9ecf2a55a92a114576144
AJ Henley <ajhenley@gmail.com>
2017-08-13 01:03:35 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/repositories/ActorRepository.java
@@ -0,0 +1,8 @@
+package com.example.demo.repositories;
+
+import com.example.demo.models.Actor;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ActorRepository extends CrudRepository<Actor, Long>{
+
+}
